959966a65bb259d0fa1fedb4bd8b09f1d1c4c3ef
Kropeks <128617034+Kropeks@users.noreply.github.com>
2023-12-19 02:19:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function validateLogin() {
 
     if (username === validUsername && password === validPassword) {
         // Authentication successful, redirect to another file (e.g., dashboard.html)
-        window.location.href = "index.html";
+        window.location.href = "main.html";
     } else {
         alert("Invalid username or password. Please try again.");
     }
